c4546b68b2295d690e8cd62a1499233e6f761230
Jesper Jarlskov <jesper@jarlskov.dk>
2017-03-08 21:57:35 +0100

    Remove prepended text from archive pages

--- functions.php
+++ functions.php
@@ -132,3 +132,18 @@ function add_custom_post_types_to_rss_feed( $args ) {
     return $args;
 }
 add_filter( 'request', 'add_custom_post_types_to_rss_feed' );
+
+add_filter( 'get_the_archive_title', function ($title) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = get_the_author();
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    }
+
+    return $title;
+});
+
